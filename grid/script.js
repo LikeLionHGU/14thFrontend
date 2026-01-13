7c4af4e686e30f9f8b951fa44923dd33e4c8b1c0
sintanjin <z01230z12383@gmail.com>
2026-01-13 19:05:44 +0900

    grid complete.ver

--- grid/script.js
+++ grid/script.js
@@ -1,8 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
     const swiper = new Swiper(".swiper", {
-        slidesPerView: "auto",
+        slidesPerView: 'auto',
         centeredSlides: true,
+
         loop: true,
+
         grabCursor: true,
         spaceBetween: 100,
         effect: "coverflow",
@@ -15,7 +17,9 @@ document.addEventListener("DOMContentLoaded", () => {
 
         coverflowEffect: {
             rotate: -25,
+            stretch: 0,
             depth: 120,
+            modifier: 1,
             slideShadows: false,
         },
 
@@ -24,9 +28,13 @@ document.addEventListener("DOMContentLoaded", () => {
             onlyInViewport: false,
         },
 
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
         pagination: {
             el: ".swiper-pagination",
-            type: "bullets",
+            type: 'bullets',
             clickable: true,
         },
     });
